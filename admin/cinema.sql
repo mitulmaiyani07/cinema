2fab71a203d87ce5c6b1c39e25a37e964ce4972a
mitulmaiyani07 <mitpatel0720@gmail.com>
2023-09-21 22:17:14 +0530

    DB UPDATE

--- admin/cinema.sql
+++ admin/cinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2023 at 08:32 PM
+-- Generation Time: Sep 21, 2023 at 06:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,6 +30,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `movie` (
   `id` int(10) NOT NULL,
   `movie_name` varchar(100) NOT NULL,
+  `movie_image` varchar(200) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
   `category_id` int(20) DEFAULT NULL,
   `release_date` datetime NOT NULL,
   `is_released` tinyint(1) NOT NULL,
@@ -50,6 +52,8 @@ CREATE TABLE `movie` (
 CREATE TABLE `movie_category` (
   `id` int(10) NOT NULL,
   `category_name` varchar(100) NOT NULL,
+  `cat_image` varchar(200) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
   `created_by` int(10) DEFAULT NULL,
   `updated_by` int(10) DEFAULT NULL,
   `created_at` datetime NOT NULL,
@@ -80,6 +84,14 @@ CREATE TABLE `users` (
   `is_deleted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `email`, `profile`, `password`, `phone_no`, `created_by`, `updated_by`, `user_type_id`, `created_at`, `updated_at`, `is_active`, `is_deleted`) VALUES
+(1, 'Mitul Maiyani', 'mitpatel0720@gmail.com', '', 'MIT0720', '9409002090', NULL, 1, 1, '2023-09-20 21:16:14', '2023-09-21 21:56:10', 1, 0),
+(2, 'Kevin Kotadiya', 'kevinkotadiya428@gmail.com', '', '999', '9510935250', NULL, 2, 1, '2023-09-21 22:11:48', '2023-09-21 22:13:41', 1, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -90,6 +102,15 @@ CREATE TABLE `user_type` (
   `id` int(10) NOT NULL,
   `type_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`id`, `type_name`) VALUES
+(1, 'admin'),
+(2, 'assistant'),
+(3, 'customer');
 
 --
 -- Indexes for dumped tables
@@ -147,13 +168,23 @@ ALTER TABLE `movie_category`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_type`
 --
 ALTER TABLE `user_type`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_user_type_id` FOREIGN KEY (`user_type_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
